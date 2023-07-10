55cbb9a83f9da4e77c21d6b2578b226b8f85dcc5
Rocky-t <78757352+Rocky-t@users.noreply.github.com>
2023-07-10 12:01:45 -0400

    make tools ambiguous

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ I enjoy antiquing and collecting watches, but as a student I seldom have the tim
 
 The data_management folder and models folder contain the early makings of a large NN for classifying watch images. With keyword search, all listings in which sellers aren't sure what they have are lost. I've created a database of nearly 100,000 labeled images and am working on a CNN classifier by brand.
 
+# Use this yourself
+
+The poll.py file is the only file that needs to be run for the bot to run in perpetuity. Replace with your keywords and your telegram chat_id and bot's unique token. The link to generate those is in that file.
+
 <img src="images/watchbot.PNG" alt="watchbot in action" width="400"/>
--- poll.py
+++ poll.py
@@ -3,10 +3,15 @@ import time
 
 #run this file if you want watchbot to run in perpituity
 
-TOKEN = "6357966637:AAEUtPeFnMUoNkJZFzg5C3NzanFddxD8vJw"
-CHAT_ID = "5928330088"
-KW = {"bulova", "seiko"}
+
+# Look into how to make a Telegram watchbot link: https://www.freecodecamp.org/news/how-to-create-a-telegram-bot-using-python/
+# After you have made your bot, plug in the TOKEN and CHAT_ID variables here
+# 
+
+TOKEN = "YOUR BOT'S UNIQUE TOKEN"
+CHAT_ID = "YOUR CHAT ID"
 KWD = {
+        # replace these with the keywords you would like to search for
        "bulova":50,
        "seiko":100,
        "rolex":1200,
@@ -17,8 +22,11 @@ KWD = {
        "lot":100,
       }
 
+#replace these with keywords you don't want results from
 NEGATIVE_KWS = {"women","women's","lady","lady's","quartz","ladies","womens","mickey"}
 
+#replace this with the ebay page you would like to scrape from
+#customize location, items per page, and any other configs on ebay before copying link
 EBAY = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=watch&_sacat=0&_sop=10&LH_BIN=1&_fsrp=1&rt=nc&LH_PrefLoc=3&_ipg=240"
 dh = deal_hunter(TOKEN,
                     CHAT_ID,
